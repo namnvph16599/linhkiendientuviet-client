73192f8872a3f99769d52e9d7e43bf1b4f304578
truongnqph14659 <truongrubi717@gmail.com>
2023-05-24 23:51:55 +0700

    first commit

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 import React from "react";
-import { useTranslation } from 'next-i18next';
 type Props = {};
 
 const Home = () => {
-  const { t } = useTranslation();
-  console.log(t);
-  return <div>home home {t('viet_nam')}</div>;
+  return <div>client</div>;
 };
 
 export default Home;
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config')
 const path = require("path");
 const nextConfig = {
-  i18n,
   transpilePackages: ["antd-mobile"],
   swcMinify: true,
   experimental: {
