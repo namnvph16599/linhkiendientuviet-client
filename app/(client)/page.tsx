72d86b1f47bfd5380fd1699ce7174e68781a7148
truongnqph14659 <truongrubi717@gmail.com>
2023-05-25 17:30:46 +0700

    add more setup

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,9 +1,18 @@
 'use client';
-import React from "react";
+import React from 'react';
+import { Button } from 'antd';
+import Link from 'next/link';
 type Props = {};
 
 const Home = () => {
-  return <div>client</div>;
+  return <div>client
+    <Link href="/product">
+      <Button type="primary" loading>
+        {' '}
+            product
+      </Button>
+    </Link>
+  </div>;
 };
 
 export default Home;
